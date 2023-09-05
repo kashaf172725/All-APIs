c81129b6d5025c8c913c9ac0ff35dd764dfc518f
kashaf172725 <134833348+kashaf172725@users.noreply.github.com>
2023-09-04 21:19:38 -0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class _MyAppState extends State<MyApp> {
       child: MaterialApp(
        
          theme: ThemeData(primarySwatch: Colors.grey,),
-      //  home:HomeScreen()
+      
         // home: SplashPage(),
            home:Test(),
       
